2d73dbf67fd073f3285346e99864e2df3154cd20
Dorota <dorota543@o2.pl>
2024-11-27 21:24:19 +0100

    added file single.php

--- wp-content/themes/mojmotyw/single.php
+++ wp-content/themes/mojmotyw/single.php
@@ -5,7 +5,9 @@ if ( have_posts() ) {
 
 	while ( have_posts() ) {
 		the_post();
-            the_title( sprintf( '<h2 class="entry-title"><a href="%s">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+            ?>
+            <?php the_title('<h2 class="entry-title">', '</h2>' ); 
+            ?>
             <h4>Kategoria: <?php the_category(); ?></h4>
             <h4>Data publikacji: <?php the_time('j F Y'); ?></h4>
             <p><?php the_content(); ?></p>
